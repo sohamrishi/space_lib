9a881fb2ca71715abf9ea0ba537c949941388d4d
sohamrishi <115031252+sohamrishi@users.noreply.github.com>
2023-08-22 21:45:11 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.toggle("right-panel-active");
+});
+
